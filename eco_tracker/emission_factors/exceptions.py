c5b5ff236cb4c0cd4bb252a1f174c3e8cd258459
Albert Szulc <44374291+albiosz@users.noreply.github.com>
2025-04-30 21:01:58 +0200

    add more specific exception + add all available units (#25)
    - the exceptions should simplify debugging
    - all available in climatiq units were added to avoid unexpected errors

--- eco_tracker/emission_factors/exceptions.py
+++ eco_tracker/emission_factors/exceptions.py
@@ -6,6 +6,12 @@ class EmissionFactorNotFound(Exception):
 		def __str__(self):
 				return f"No emission factor found for activity_id: {self.activity_id}"
 
+class EmissionFactorNotFoundForProduct(Exception):
+		def __init__(self, product_name: str, tried_search_queries: list[str]):
+				self.product_name = product_name
+				self.tried_search_queries = tried_search_queries
+				super().__init__(f"Emission factor not found for product named: {product_name}. Tried search queries: {tried_search_queries}")
+
 class EmissionFactorInfoNotFound(Exception):
 		def __init__(self, search_query: str):
 				self.search_query = search_query
@@ -13,3 +19,11 @@ class EmissionFactorInfoNotFound(Exception):
 
 		def __str__(self):
 				return f"No emission factor info found for search_query: {self.search_query}"
+  
+class UnsupportedUnit(Exception):
+		def __init__(self, unit: str):
+				self.unit = unit
+				super().__init__(f"Unsupported unit: {unit}")
+
+		def __str__(self):
+			return f"Unsupported unit: {self.unit}"
--- eco_tracker/emission_factors/unit.py
+++ eco_tracker/emission_factors/unit.py
@@ -1,25 +1,87 @@
 # https://www.climatiq.io/docs/api-reference/unit-types
 # TODO: add all available unit types
+from eco_tracker.emission_factors.exceptions import UnsupportedUnit
+
+
 def return_unit(unit_type: str) -> dict[str, str] | None:
 
 	match unit_type.lower():
-		case "weight":
-			return {
-				'mass_unit': 'kg'
-			}
-		case "money":
-			return {
-				'money_unit': 'eur'
-			}
 		case "area":
 			return {
 				'area_unit': 'm2'
+			}
+		case "areaovertime":
+			return {
+				'mass_unit': 'kg',
+				'time_unit': 'h'
+			}
+		case 'containeroverdistance':
+			return {
+				'distance_unit': 'km',
+			}
+		case 'data':
+			return {
+				'data_unit': 'GB'
+			}
+		case 'dataovertime':
+			return {
+				'data_unit': 'GB',
+				'time_unit': 'hour'
 			}
 		case "distance":
 			return {
 				'distance_unit': 'km'
 			}
+		case 'distanceovertime':
+			return {
+				'distance_unit': 'km',
+				'time_unit': 'hour'
+			}
+		case 'energy':
+			return {
+				'energy_unit': 'kWh'
+			}
+		case "money":
+			return {
+				'money_unit': 'eur'
+			}
 		case "number":
 			return {}
+		case 'numberovertime':
+			return {
+				'time_unit': 'h'
+			}
+		case 'pasesengeroverdistance':
+			return {
+				'distance_unit': 'km',
+			}
+		case 'power':
+			return {
+				'power_unit': 'kW'
+			}
+		case 'time':
+			return {
+				'time_unit': 'hour'
+			}
+		case 'volume':
+			return {
+				'volume_unit': 'l'
+			}
+		case 'weight':
+			return {
+				'weight_unit': 'kg'
+			}
+		case 'weightoverdistance':
+			return {
+				'distance_unit': 'km',
+				'weight_unit': 'kg'
+			}
+		case 'weightovertime':
+			return {
+				'time_unit': 'hour',
+				'weight_unit': 'kg'
+			}
+		case "":
+			return None
 
-	raise NotImplementedError
+	raise UnsupportedUnit(unit_type)
