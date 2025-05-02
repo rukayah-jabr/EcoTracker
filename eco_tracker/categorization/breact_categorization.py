21d5f0ae15cb4297a48017084333a1062a63f8be
rukayah-jabr <rukayah.jabr@stud.fh-campuswien.ac.at>
2025-05-02 12:27:26 +0200

    resolved Alberts comments

--- eco_tracker/categorization/breact_categorization.py
+++ eco_tracker/categorization/breact_categorization.py
@@ -17,8 +17,8 @@ class BreactCategorizer(Categorizer):
 
 	#implemet the method from interface	(Categorizer)
 	def generate_categorization(self, product: str, num_of_categories: int = 10) -> list:
-		api_url_post = 'https://api-os.breact.ai/api/v1/services/classifier/process'
-		api_url_get = 'https://api-os.breact.ai/api/v1/services/result'
+		api_url_classifier = 'https://api-os.breact.ai/api/v1/services/classifier/process'
+		api_url_result = 'https://api-os.breact.ai/api/v1/services/result'
 
 		request_data = {
 			"content": product,
@@ -39,7 +39,7 @@ class BreactCategorizer(Categorizer):
 		}
 
 		#make the post request
-		response_post = requests.post(api_url_post, json=request_data, headers=self.headers)
+		response_post = requests.post(api_url_classifier, json=request_data, headers=self.headers)
 
 		if response_post.status_code != HTTPStatus.OK:
 			raise exceptions.HTTPException(response_post.status_code, response_post.text)
@@ -53,7 +53,7 @@ class BreactCategorizer(Categorizer):
 		time.sleep(5)
 
 		#make the get request
-		get_url = f'{api_url_get}/{process_id}?access_token={access_token}'
+		get_url = f'{api_url_result}/{process_id}?access_token={access_token}'
 		response_get = requests.get(get_url, headers=self.headers)
 		if response_get.status_code != HTTPStatus.OK:
 			raise exceptions.HTTPException(response_get.status_code, response_get.text)
